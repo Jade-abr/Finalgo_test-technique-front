a4fe40d2554e46071cfd21171ff378e37c3a26f8
Jade-abr <jade@finalgo.fr>
2021-06-14 11:37:53 +0200

    in progress

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { MenuComponent } from './dashboard/menu/menu.component';
+import { InformationsComponent } from './dashboard/informations/informations.component';
+import { ProcessComponent } from './dashboard/process/process.component';
+import { ButtonComponent } from './button/button.component';
+import { ProgressCercleComponent } from './dashboard/process/progress-cercle/progress-cercle.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DashboardComponent,
+    MenuComponent,
+    InformationsComponent,
+    ProcessComponent,
+    ButtonComponent,
+    ProgressCercleComponent,
   ],
   imports: [
     BrowserModule,
---
+++ src/app/dashboard/process/progress-cercle/progress-cercle.component.ts
@@ -0,0 +1,24 @@
+import {Component, Input, OnInit} from '@angular/core';
+
+@Component({
+  selector: 'app-progress-cercle',
+  templateUrl: './progress-cercle.component.html',
+  styleUrls: [
+    './../../../../css/bootstrap.min.css',
+    './progress-cercle.component.scss'
+  ]
+
+})
+export class ProgressCercleComponent implements OnInit {
+
+  @Input() caption: string;
+  @Input() colorBootstrapClass: string;
+  @Input() progressbarClass: string;
+  @Input() starColor: string;
+  @Input() maincercleColor: string;
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
